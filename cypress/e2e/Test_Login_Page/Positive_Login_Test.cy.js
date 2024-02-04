5002645731b9f38d46c322a584b6639f03851c7e
Ayhan Metin <108268012+ayhanmetin@users.noreply.github.com>
2024-02-04 23:12:04 +0100

    Positive_Login_Test

---
+++ cypress/e2e/Test_Login_Page/Positive_Login_Test.cy.js
@@ -0,0 +1,18 @@
+describe('Positive LogIn Test', () => {
+  it('successfully logs in the user and shows logout button', () => {
+    cy.visit('https://www.ayhanmetin.eu/practice/loginpage');
+
+    // Enter username and password
+    cy.get('input[type="text"]').type('ayhan89');
+    cy.get('input[type="password"]').type('ayhan123');
+
+    // Click the submit button to log in
+    cy.get('button[type="submit"]').click();
+
+    // Wait for the login success message to appear before checking for the log out button
+    cy.contains('Login Successful!').should('be.visible');
+
+    // Now check for the "Log out" button
+    cy.contains('button', 'Log out', { timeout: 10000 }).should('be.visible');
+  });
+});
